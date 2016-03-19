81086edd089b0d1512526d5187adb8c49e134040
akshaybhalotia <akshay.bhalotia@razorpay.com>
2016-03-19 11:54:49 +0530

    Framework iPhone only app

---
+++ Razorpay Sample App/Razorpay Sample App/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Razorpay Sample App
+//
+//  Created by Akshay Bhalotia on 18/03/16.
+//  Copyright © 2016 Akshay Bhalotia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Razorpay Sample App/Razorpay Sample App/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Razorpay Sample App
+//
+//  Created by Akshay Bhalotia on 18/03/16.
+//  Copyright © 2016 Akshay Bhalotia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+  }
+}
