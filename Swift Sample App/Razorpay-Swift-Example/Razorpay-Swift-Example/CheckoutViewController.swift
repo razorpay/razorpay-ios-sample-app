32f17b2af75a588be0c38d44bbcf84b23ed65b1f
sachin nautiyal <sachinnautiyal00@gmail.com>
2020-07-08 18:49:04 +0530

    Feat: Added New UI + Icons

--- Swift Sample App/Razorpay-Swift-Example/Razorpay-Swift-Example/CheckoutViewController.swift
+++ Swift Sample App/Razorpay-Swift-Example/Razorpay-Swift-Example/CheckoutViewController.swift
@@ -1,51 +1,52 @@
 //
-//  ViewController.swift
+//  CheckoutViewController.swift
 //  Razorpay-Swift-Example
 //
-//  Created by Sachin Nautiyal on 31/01/20.
+//  Created by Sachin Nautiyal on 08/07/20.
 //  Copyright © 2020 Razorpay. All rights reserved.
 //
 
 import UIKit
 import Razorpay
 
-class ViewController : UIViewController {
+class CheckoutViewController: UIViewController {
     
     var razorpayObj : RazorpayCheckout? = nil
-    let defaultHeight : CGFloat = 40
-    let defaultWidth : CGFloat = 120
+    var merchantDetails : MerchantsDetails = MerchantsDetails.getDefaultData()
     
-    let razorpayKey = "" // Sign up for a Razorpay Account(https://dashboard.razorpay.com/#/access/signin) and generate the API Keys(https://razorpay.com/docs/payment-gateway/dashboard-guide/settings/#api-keys/) from the Razorpay Dashboard.
+    let razorpayKey = "rzp_test_1DP5mmOlF5G5ag" // Sign up for a Razorpay Account(https://dashboard.razorpay.com/#/access/signin) and generate the API Keys(https://razorpay.com/docs/payment-gateway/dashboard-guide/settings/#api-keys/) from the Razorpay Dashboard.
     
     override func viewDidLoad() {
         super.viewDidLoad()
-//        self.setupCheckoutButton()
     }
+    
     
     // Don't use viewDidLoad method for calling open method, use viewWillAppear instead,
     // as it will give you this error - "Warning: Attempt to present < finishViewController: 0x1e56e0a0 > on < ViewController: 0x1ec3e000> whose view is not in the window hierarchy!".
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
+        self.setupLogo()
     }
     
-    func setupCheckoutButton() {
-        let buyButton = UIButton(type: .custom)
-        buyButton.setTitle("Checkout", for: .normal)
-        buyButton.setTitleColor(.blue, for: .normal)
-        self.view.addSubview(buyButton)
-        buyButton.translatesAutoresizingMaskIntoConstraints = false
-        NSLayoutConstraint.activate([
-            buyButton.heightAnchor.constraint(equalToConstant: self.defaultHeight),
-            buyButton.widthAnchor.constraint(equalToConstant: self.defaultWidth),
-            buyButton.topAnchor.constraint(equalTo: view.topAnchor, constant: 200),
-            buyButton.centerXAnchor.constraint(equalTo: view.centerXAnchor)
-        ])
-        buyButton.addTarget(self, action: #selector(openCheckoutAction(_:)), for: .touchUpInside)
+    func setupLogo() {
+        let image = #imageLiteral(resourceName: "Logo")
+        navigationItem.titleView = UIImageView(image: image)
     }
     
-    @IBAction func openCheckoutAction(_ sender: UIButton) {
+    @IBAction func payNowButtonAction(_ sender: UIButton) {
         self.openRazorpayCheckout()
     }
+    
+    @IBAction func settingButtonAction(_ sender: Any) {
+        self.performSegue(withIdentifier: "toSettingsSegue", sender: nil)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let controller = segue.destination as? SettingsTableViewController {
+            controller.delegate = self
+        }
+    }
+    
     
     private func openRazorpayCheckout() {
         // 1. Initialize razorpay object with provided key. Also depending on your requirement you can assign delegate to self. It can be one of the protocol from RazorpayPaymentCompletionProtocolWithData, RazorpayPaymentCompletionProtocol.
@@ -54,16 +55,14 @@ class ViewController : UIViewController {
             "prefill": [
                 "contact": "1234567890",
                 "email": "a@a.com"
-                //                "method":"wallet",
-                //                "wallet":"amazonpay"
             ],
-            "image": "http://www.freepngimg.com/download/light/2-2-light-free-download-png.png",
+            "image": merchantDetails.logo,
             "amount" : 100,
-            "timeout":10,
+            "name": merchantDetails.name,
             "theme": [
-                "color": "#F37254"
-            ]//            "order_id": "order_B2i2MSq6STNKZV"
-            // and all other options
+                "color": merchantDetails.color.toHexString()
+            ]
+            // follow link for more options - https://razorpay.com/docs/payment-gateway/web-integration/standard/checkout-form/
         ]
         if let rzp = self.razorpayObj {
             rzp.open(options)
@@ -73,8 +72,16 @@ class ViewController : UIViewController {
     }
 }
 
+extension CheckoutViewController : CustomizeDataDelegate {
+    
+    func dataChanged(with merchantDetails: MerchantsDetails) {
+        self.merchantDetails = merchantDetails
+    }
+    
+}
+
 // RazorpayPaymentCompletionProtocol - This will execute two methods 1.Error and 2. Success case. On payment failure you will get a code and description. In payment success you will get the payment id.
-extension ViewController : RazorpayPaymentCompletionProtocol {
+extension CheckoutViewController : RazorpayPaymentCompletionProtocol {
     
     func onPaymentError(_ code: Int32, description str: String) {
         print("error: ", code, str)
@@ -88,7 +95,7 @@ extension ViewController : RazorpayPaymentCompletionProtocol {
 }
 
 // RazorpayPaymentCompletionProtocolWithData - This will returns you the data in both error and success case. On payment failure you will get a code and description. In payment success you will get the payment id.
-extension ViewController: RazorpayPaymentCompletionProtocolWithData {
+extension CheckoutViewController: RazorpayPaymentCompletionProtocolWithData {
     
     func onPaymentError(_ code: Int32, description str: String, andData response: [AnyHashable : Any]?) {
         print("error: ", code)
@@ -102,7 +109,7 @@ extension ViewController: RazorpayPaymentCompletionProtocolWithData {
 }
 
 
-extension ViewController {
+extension CheckoutViewController {
     func presentAlert(withTitle title: String?, message : String?) {
         DispatchQueue.main.async {
             let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
